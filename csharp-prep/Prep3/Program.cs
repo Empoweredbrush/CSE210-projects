e9b12ed2fdedb5c25ed3be2c6481e4d6d660165a
Empoweredbrush <73969325+Empoweredbrush@users.noreply.github.com>
2023-01-16 15:04:12 -0700

    Assignment for Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,26 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 11);
+
+        int guess = -1;
+        while (guess != magicNumber) {
+            Console.WriteLine("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+            if (guess == magicNumber) {
+                Console.WriteLine("You got it!");
+            }
+            else if (guess > magicNumber) {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber) {
+                Console.WriteLine("Higher");
+            }
+            else {
+                Console.WriteLine("Input invalid");
+            }
+        }
     }
 }
