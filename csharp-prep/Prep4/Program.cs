600650e17e17668220c3728b5d25f65afa6d77df
Empoweredbrush <73969325+Empoweredbrush@users.noreply.github.com>
2023-01-25 12:53:05 -0700

    Work for Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers;
+        numbers = new List<int>();
+        int listNumber = 1;
+        while (listNumber != 0) {
+            Console.WriteLine("Please enter a number. Enter 0 when finished.");
+            listNumber = int.Parse(Console.ReadLine());
+            if (listNumber != 0) {
+                numbers.Add(listNumber);
+            }
+            else if (listNumber == 0) {
+                break;
+            }
+            else {
+                Console.WriteLine("Put in valid number.");
+            }
+        }
+
+        int listSum = numbers.Sum();
+        Console.WriteLine($"The sum is: {listSum}");
+
+        double listAverage = numbers.Average();
+        Console.WriteLine($"The average is: {listAverage}");
+
+        int listMax = numbers.Max();
+        Console.WriteLine($"The largest number is: {listMax}");
     }
 }
