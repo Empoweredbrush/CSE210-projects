09fc7cbb507709e1431d3ae3b3e762680d9ddf42
Empoweredbrush <73969325+Empoweredbrush@users.noreply.github.com>
2023-01-26 14:01:16 -0700

    Programs for learning 02 assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Apple";
+        job1._startYear = 2028;
+        job1._endYear = 2035;
+
+        Job job2 = new Job();
+        job2._company = "Santa Monica Studios";
+        job2._jobTitle = "Graphic Designer";
+        job2._startYear = 2035;
+        job2._endYear = 2045;
+
+        job1.Display();
+        job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Shon Ashby";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayResume();
+
+        
     }
 }
