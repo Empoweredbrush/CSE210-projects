bbcd2dd016206b5f4fbf3d9180f58c21b7438348
Empoweredbrush <73969325+Empoweredbrush@users.noreply.github.com>
2023-02-13 13:24:44 -0700

    Assignment for learning activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment math1 = new MathAssignment("Joseph Prep", "Science", "Section 4.5", "Problemts 1-20");
+        Assignment schoolWork = new Assignment("Shon Ashby", "Computer Science");
+        WritingAssignment writing1 = new WritingAssignment("SHon Ashby", "ENglish", "WHy you should buy a Walker Mower");
+        Console.WriteLine(schoolWork.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+        Console.WriteLine(writing1.GetWritingInformation());
+
+
+
     }
 }
