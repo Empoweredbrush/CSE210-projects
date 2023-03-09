5bab00278e5f1d3a3a37881101d4fccbe286971d
Empoweredbrush <73969325+Empoweredbrush@users.noreply.github.com>
2023-03-09 15:50:56 -0700

    Learning activity for Polymorphism

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        
+        Square square1 = new Square("blue", 5.2);
+        Rectangle rectangle1 = new Rectangle("green", 2.5, 3);
+        Circle circle1 = new Circle("Red", 3.15);
+
+        shapes.Add(square1);
+        shapes.Add(rectangle1);
+        shapes.Add(circle1);
+
+        foreach(Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
     }
 }
